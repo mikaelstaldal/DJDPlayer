c89c988eb26dab61ed4820b1c56e933231367526
mikaelstaldal <mikael@staldal.nu>
2013-06-06 10:04:47 +0200

    Layout fixes

--- src/nu/staldal/djdplayer/MusicUtils.java
+++ src/nu/staldal/djdplayer/MusicUtils.java
@@ -539,7 +539,7 @@ public class MusicUtils {
         }
 
         a.setTitle(title);
-        View v = a.findViewById(R.id.sd_error);
+        View v = a.findViewById(R.id.sd_message);
         if (v != null) {
             v.setVisibility(View.VISIBLE);
         }
@@ -552,7 +552,7 @@ public class MusicUtils {
     }
 
     public static void hideDatabaseError(Activity a) {
-        View v = a.findViewById(R.id.sd_error);
+        View v = a.findViewById(R.id.sd_message);
         if (v != null) {
             v.setVisibility(View.GONE);
         }
