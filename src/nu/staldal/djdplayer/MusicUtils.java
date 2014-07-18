0d108b777cec91a06316401e66df91ba71a09487
Mikael Ståldal <mikael@staldal.nu>
2014-07-18 17:00:32 +0200

    Ignore inspection

--- src/nu/staldal/djdplayer/MusicUtils.java
+++ src/nu/staldal/djdplayer/MusicUtils.java
@@ -658,6 +658,7 @@ public class MusicUtils {
     static boolean isLong(String s) {
         if (s == null) return false;
         try {
+            //noinspection ResultOfMethodCallIgnored
             Long.parseLong(s);
             return true;
         } catch (NumberFormatException e) {
