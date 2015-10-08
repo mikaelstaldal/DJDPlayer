f403ffdd1ade5c54ea151326e5f0b5c9a793ccdd
Mikael Ståldal <mikael@staldal.nu>
2015-10-08 21:46:57 +0200

    Fix typo

--- src/main/java/nu/staldal/djdplayer/MusicUtils.java
+++ src/main/java/nu/staldal/djdplayer/MusicUtils.java
@@ -680,7 +680,7 @@ public class MusicUtils {
     }
 
     static boolean android44OrLater() {
-        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
     }
 
     static boolean hasMenuKey(Context context) {
